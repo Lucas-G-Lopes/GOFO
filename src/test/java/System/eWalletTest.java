284289b25f040a59ffec84952b1344f082f4ab4d
lucas <lucasmedeiros2001@hotmail.com>
2023-11-24 11:34:13 -0300

    Inclusao dos testes da classe eWallet

---
+++ src/test/java/System/eWalletTest.java
@@ -0,0 +1,41 @@
+package System;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class eWalletTest {
+    private eWallet eWallet;
+
+    @BeforeEach
+    void setUp() {
+        eWallet = new eWallet();
+    }
+
+    @Test
+    void setBalance() {
+        eWallet.setBalance(1000);
+        assertEquals(1000,eWallet.getBalance());
+    }
+
+    @Test
+    void deposit() {
+        eWallet.setBalance(1000);
+        eWallet.deposit(500);
+        assertEquals(1500,eWallet.getBalance());
+    }
+
+    @Test
+    void withdraw() {
+        eWallet.setBalance(1000);
+        eWallet.withdraw(500);
+        assertEquals(500,eWallet.getBalance());
+    }
+
+    @Test
+    void getBalance() {
+        eWallet.setBalance(1000);
+        assertEquals(1000,eWallet.getBalance());
+    }
+}
